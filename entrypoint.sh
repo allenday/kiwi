d024f8671b8f0d246d0b79b8e102402283c2e493
Allen Day <allenday@allenday.com>
2017-04-30 23:31:39 -0700

    verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 perl -pi -e 's/irc.kiwiirc.com/'$WEBIRC_NETWORK'/' /opt/KiwiIRC/config.js
 perl -pi -e 's/"127.0.0.1":\s+"foobar"/"'$WEBIRC_NETWORK'": "'$WEBIRC_PASSWORD'"/' /opt/KiwiIRC/config.js
 
-/opt/KiwiIRC/kiwi -f start -c /opt/KiwiIRC/config.js
+/opt/KiwiIRC/kiwi -f start -c /opt/KiwiIRC/config.js -v
